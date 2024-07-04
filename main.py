1043dff909663bdd9306a0e3548d3dcd1235171a
Thaviab <abthaveesh@gmail.com>
2024-07-04 11:50:15 +1000

    game_logic_1.1

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class Player():
         self.all_cards = []
         
     def remove_one(self):
-        return self.add_cards.pop(0)
+        return self.all_cards.pop(0)
     
     def add_cards(self, new_cards):
         if type(new_cards) == type([]):
@@ -85,5 +85,29 @@ if __name__ == '__main__':
         player_two_cards.append(player_two.remove_one())
         
         #while at war
-        
-        
+        at_war = True
+        while at_war:
+            if player_one_cards[-1].value > player_two_cards[-1].value:
+                player_one.add_cards(player_one_cards)
+                player_one.add_cards(player_two_cards)
+                break
+            elif player_one_cards[-1].value < player_two_cards[-1].value:
+                player_two.add_cards(player_one_cards)
+                player_two.add_cards(player_two_cards)
+                break
+            else:
+                print("WAR!")
+                if len(player_one.all_cards) < 5:
+                    print("Player One unable to declare war!")
+                    print("Player two wins")
+                    game_on = False
+                    break
+                elif len(player_two.all_cards) < 5:
+                    print("Player Two unable to declare war!")
+                    print("Player one wins")
+                    game_on = False
+                    break
+                else:
+                    for num in range(5):
+                        player_one_cards.append(player_one.remove_one())
+                        player_two_cards.append(player_two.remove_one())
