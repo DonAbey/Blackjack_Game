896d35509363d58d23ee9273940c1faf60f69997
Thaviab <abthaveesh@gmail.com>
2024-07-04 10:56:13 +1000

    game logic

--- main.py
+++ main.py
@@ -27,10 +27,63 @@ class Deck:
     
     def shuffle(self):
         random.shuffle(self.all_cards)
+        
+    def deal_one(self):
+        return self.all_cards.pop()
 
-
+class Player():
+    def __init__(self,name):
+        self.name = name
+        self.all_cards = []
+        
+    def remove_one(self):
+        return self.add_cards.pop(0)
+    
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+    
+    def __str__(self) -> str:
+        return f"Player {self.name} has {len(self.all_cards)} cards"
+        
+        
 
 if __name__ == '__main__':
+    player_one = Player("One")
+    player_two = Player("Two")
+    
     new_deck = Deck()
-    for card in new_deck.all_cards:
-        print(card)
+    new_deck.shuffle()
+    
+    for x in range(26):
+        player_one.add_cards(new_deck.deal_one())
+        player_two.add_cards(new_deck.deal_one())
+        
+    game_on = True
+    
+    round_num = 0
+    while game_on:
+        round_num += 1
+        print(f"Round {round_num}")
+        
+        if len(player_one.all_cards) == 0:
+            print("Player One, out of cards! Player Two wins")
+            game_on = False
+            break
+        elif len(player_two.all_cards) == 0:
+            print("Player Two, out of cards! Player One wins")
+            game_on = False
+            break
+        
+        #New Round
+        player_one_cards = []
+        player_one_cards.append(player_one.remove_one())
+        
+        player_two_cards = []
+        player_two_cards.append(player_two.remove_one())
+        
+        #while at war
+        
+        
